5ee15afda3ffa96890a623cd9e2664734896192c
ines-besrour <inesbes2001@gmail.com>
2024-04-14 16:11:12 +0100

    creation du projet

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { InternshipDetails } from './components/InternshipDetails'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+          Hello world!
+      </h1>
+      <InternshipDetails />
+    </div>
+
+  )
+}
+export default App
