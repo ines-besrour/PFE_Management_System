625c48ceae2cfb6721bc59ca34a0601fd5b4e05a
HajjiRoua <roua2001hajji@gmail.com>
2024-04-21 12:22:22 +0100

    feat:add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import './App.css'
 import { Footer } from './components/Footer'
+import {Menu} from "./components/Menu";
+import {InternshipDetails} from "./pages/InternshipDetails";
 import {Header} from "./components/Header";
-import {Home} from "./pages/home";
 
 function App() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <Header/>
-      <Home/>
-      <Footer/>
+        <Header/>
+        {/*<Home/>*/}
+        <div className="flex flex-row flex-grow">
+            <Menu/>
+            <InternshipDetails/>
+            {/*<StudentInformations/>*/}
+            {/*<Documents/>*/}
+        </div>
+        <Footer/>
     </div>
 
   )
